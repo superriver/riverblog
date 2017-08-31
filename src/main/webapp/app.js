38a39a5639d7aa0329faf2d32951a266e08c83ae
river123 <river6025@163.com>
2017-08-31 17:23:14 +0800

    ###

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -3,17 +3,21 @@
  */
 
 var app = angular.module("webApp", ["ngRoute",
-		"ui.bootstrap", "ngSanitize", "ui.router", "ngFileUpload" ]);
+    "ui.bootstrap", "ngSanitize", "ui.router", "ngFileUpload"]);
 
 // app.config(function($httpProvider) {
 //     $httpProvider.interceptors.push('HttpInterceptor');
 // });
-app.config(function($routeProvider,$locationProvider){
-	$routeProvider.when("/",
-			{
-				templateUrl:"modules/index/index.view.html",
-				controller:'IndexController'
-			}
-		);
+app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider.when("/",
+        {
+            templateUrl: "modules/index/index.view.html",
+            controller: 'IndexController'
+        }
+    ).when("/blog",
+        {
+            templateUrl: "modules/blog/article.view.html",
+            controller: 'ArticleController'
+        });
     $locationProvider.hashPrefix('!');
 });
